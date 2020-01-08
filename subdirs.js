f65ece4fe715f7fe2798d59f123ed9abcc9dc850
Ljubomir Simin <ljubomir@coingcs.com>
2020-01-08 22:48:53 +0100

    Group multiple CD files together.

--- subdirs.js
+++ subdirs.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
-const self = __filename.slice(__dirname.length + 1);
 
 (async () => {
   console.log('start');
   const files = fs.readdirSync('.', { withFileTypes: true })
-    .filter(f => f.name !== self)
     .filter(f => !f.isDirectory())
     .map(f => f.name);
   console.log({files});
 
   files.forEach((file) => {
-    const dirname = file.substr(0, file.lastIndexOf('.')) || file;
+    const dirname = (file.substr(0, file.lastIndexOf('.')) || file).replace(/cd[0-9]/gi,'');
     console.log(file, dirname);
 
     if (!fs.existsSync(dirname)) {
