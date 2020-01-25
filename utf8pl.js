f3031f257372684fdfbb8c210c6059268f0bfbd8
Ljubomir Simin <ljubomir@coingcs.com>
2020-01-25 20:47:29 +0100

    (fix) Add size check.

--- utf8pl.js
+++ utf8pl.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const recursive = require('recursive-readdir');
 
+const maxSize = 0x4000000; // 64mb
+
 (async () => {
   console.log('start');
   const files = (await recursive('.'))
     .filter(file => file.includes('.txt') || file.includes('.srt') || file.includes('.sub'))
 
   files.forEach(file => {
+    if (fs.statSync(file).size > maxSize) return;
+    
     const utf8 = fs.readFileSync(file, 'utf-8');
     
 //(³)|(ê)|(¿)|(æ)|(¹)|(œ)|(ñ)|(Ÿ)
